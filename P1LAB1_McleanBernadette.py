9a42c5f7fb5bcc276efd42128a58c6c31a1f6d58
Bernadette Mathis Mclean <bernadettem68@gmail.com>
2025-06-16 18:48:40 -0400

    Add files via upload

---
+++ P1LAB1_McleanBernadette.py
@@ -0,0 +1,7 @@
+Python 3.13.5 experimental free-threading build (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:12:42) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=== RESTART: C:/Users/mathi/OneDrive/Music/P1LAB1_/P1LAB1_McleanBernadette.py ==
+First Name: Cynthia
+Last Name: Cupcakes
+Hello, Cynthia Cupcakes! Welcome to CTI110
