d186dd1812b8edb153a130cf61ebe6b93dedbc0a
Bernadette Mathis Mclean <bernadettem68@gmail.com>
2025-06-27 12:14:41 -0400

    Update P1LAB1_MahtisMcleanBernadette2.py
    Signed-off-by: Bernadette Mathis Mclean <bernadettem68@gmail.com>

--- P1LAB1_McleanBernadette2.py
+++ P1LAB1_McleanBernadette2.py
@@ -1,6 +1,6 @@
-first_name = "Cynthia"
-last_name = "Cupcakes"
+# first_name = "Cynthia"
+# last_name = "Cupcakes"
 
-print("First Name:", first_name)
-print("Last Name:", last_name)
-print(f"Hello, {first_name} {last_name}! Welcome to CTI110")
+# print("First Name:", first_name)
+  print("Last Name:", last_name)
+  print(f"Hello, {first_name} {last_name}! Welcome to CTI110")
