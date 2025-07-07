30bfeda21a67376e87f1cf4da97845f4a5790d79
Bernadette Mathis Mclean <bernadettem68@gmail.com>
2025-07-07 14:54:29 -0400

    P4LAB2_MathisMcleanBernadette.py
    Signed-off-by: Bernadette Mathis Mclean <bernadettem68@gmail.com>

--- P4LAB2_MathisMcleanBernadette.py
+++ P4LAB2_MathisMcleanBernadette.py
@@ -1,16 +1,22 @@
- def display_table(n):
- for i in range(1, 12):
-print(f"{n} * {i} = {n * i}")
+ def display_multiplication_table(num):
+    for i in range(1, 13):
+        print(f"{num} * {i} = {num * i}")
 
-run_program = "yes"
- while run_program.lower() == "yes":
-try:
- number = int(input("\nEnter an integer: "))
-if number >= 0:
- display_table(number)
-   else:
- # Still display table for 0 and show the message
- display_table(0)
- print("Invalid input. Please enter a valid integer.")
- run_program = input("\nWould you like to run the program again? ")
- print("Exiting Program...")
+def main():
+    run_program = "yes"
+
+    while run_program.lower() == "yes":
+        try:
+            number = int(input("Enter an integer: "))
+            if number >= 0:
+                display_multiplication_table(number)
+            else:
+                print("This program does not handle negative number.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+        run_program = input("Would you like to run the program again? ")
+        if run_program.lower() != "yes":
+            print("Exiting Program...")
+
+main()
