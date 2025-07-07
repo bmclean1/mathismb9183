7c980ca0c0d8ecd6d1abb28639c144d6c496695a
Bernadette Mathis Mclean <bernadettem68@gmail.com>
2025-07-06 22:56:32 -0400

    Update P4LAB2_MathisMcleanBernadette.py
    Signed-off-by: Bernadette Mathis Mclean <bernadettem68@gmail.com>

--- P4LAB2_MathisMcleanBernadette.py
+++ P4LAB2_MathisMcleanBernadette.py
@@ -1,22 +1,16 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
->>> def display_table(n):
-...     for i in range(1, 12):
-...         print(f"{n} * {i} = {n * i}")
-...
-... run_program = "yes"
-...
-... while run_program.lower() == "yes":
-...     try:
-...         number = int(input("\nEnter an integer: "))
-...         if number >= 0:
-...             display_table(number)
-...         else:
-...             # Still display table for 0 and show the message
-...             display_table(0)
-...
-...         print("Invalid input. Please enter a valid integer.")
-...
-...     run_program = input("\nWould you like to run the program again? ")
-...
-... print("Exiting Program...")
+ def display_table(n):
+ for i in range(1, 12):
+print(f"{n} * {i} = {n * i}")
+
+run_program = "yes"
+ while run_program.lower() == "yes":
+try:
+ number = int(input("\nEnter an integer: "))
+if number >= 0:
+ display_table(number)
+   else:
+ # Still display table for 0 and show the message
+ display_table(0)
+ print("Invalid input. Please enter a valid integer.")
+ run_program = input("\nWould you like to run the program again? ")
+ print("Exiting Program...")
